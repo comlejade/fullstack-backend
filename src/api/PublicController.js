79e95445e2789aecd8f1b1d6abdbd6488be731fb
cuijianlei <thundersmile@126.com>
2021-03-02 16:57:37 +0800

    modify 验证码配置

--- src/api/PublicController.js
+++ src/api/PublicController.js
@@ -3,7 +3,14 @@ import svgCaptcha from 'svg-captcha'
 class PublicController {
   constructor() { }
   async getCaptcha (ctx) {
-    const newCaptcha = svgCaptcha.create({})
+    const newCaptcha = svgCaptcha.create({
+      size: 4,
+      ignoreChars: '0oO1i',
+      noise: Math.floor(Math.random * 5),
+      color: true,
+      width: 150,
+      height: 50
+    })
     ctx.body = {
       code: 200,
       data: newCaptcha.data
